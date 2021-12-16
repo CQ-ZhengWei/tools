9543aae1032ea3be2fb7dc75f02c568b916ab30f
zhengwei <Administrator@CQ360DN-2021ELY>
2021-12-16 15:10:08 +0800

    1. 代码工具支持类型注释

--- EasyBusinessOnline.Tools/FormTool/CsharpToJsTable.Designer.cs
+++ EasyBusinessOnline.Tools/FormTool/CsharpToJsTable.Designer.cs
@@ -34,6 +34,9 @@ namespace EasyBusinessOnline.Tools.FormTool
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -83,12 +86,45 @@ namespace EasyBusinessOnline.Tools.FormTool
             this.textBox2.Size = new System.Drawing.Size(500, 50);
             this.textBox2.TabIndex = 8;
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(102, 356);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 16);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "行内注释";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(657, 482);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "复制C#类";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(195, 482);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "复制C#类";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // CsharpToJsTable
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(1227, 517);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -108,5 +144,8 @@ namespace EasyBusinessOnline.Tools.FormTool
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- EasyBusinessOnline.Tools/FormTool/CsharpToJsTable.cs
+++ EasyBusinessOnline.Tools/FormTool/CsharpToJsTable.cs
@@ -113,13 +113,26 @@ namespace TD.Concrete.Model.Group.Models.OutModel.Report
 ";
             text = textBox1.Text;
             if (string.IsNullOrEmpty(text)) return;
-            var allLines = text.Split(new string[] { "\r\n" },StringSplitOptions.RemoveEmptyEntries);
+            var allLines = text.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, string> fields;
+            if (checkBox1.Checked)
+            {
+                fields = Line(allLines);
+            }
+            else
+            {
+                fields = Default(allLines);
+            }
+            GetResult(fields);
+        }
+        private Dictionary<string, string> Default(string[] allLines)
+        {
             var temp = new List<string>();
             foreach (var item in allLines)
             {
                 if (string.IsNullOrEmpty(item)) continue;
                 var line = item.Trim();
-                if (new Regex("^(///[\\s\\S]+(summary|default|length|nullable)[\\s\\S]+)|(public[\\s\\S]+class[\\s\\S]+)", RegexOptions.IgnoreCase).IsMatch(line))
+                if (new Regex("^(///[\\s\\S]+(summary|default|length|nullable|<para>)[\\s\\S]+)|(public[\\s\\S]+class[\\s\\S]+)", RegexOptions.IgnoreCase).IsMatch(line))
                 {
                     continue;
                 }
@@ -130,27 +143,81 @@ namespace TD.Concrete.Model.Group.Models.OutModel.Report
             }
             Dictionary<string, string> fields = new Dictionary<string, string>();
             string desc = null;
-            string name = null;
+            string name;
             foreach (var item in temp)
             {
                 Regex regexDesc = new Regex("/// *(desc)*([\\s\\S]+)", RegexOptions.IgnoreCase);
-                Regex regexField = new Regex("public[\\s\\S]+ ([^{]+)[\\s\\S]+get[\\s\\S]+set", RegexOptions.IgnoreCase);
+                Regex regexField = new Regex("public ([\\s\\S]+) ([^{]+)[\\s\\S]+get[\\s\\S]+set", RegexOptions.IgnoreCase);
                 if (regexDesc.IsMatch(item))
                 {
                     desc = regexDesc.Matches(item)[0].Groups[2].Value.Trim();
                 }
                 else if (regexField.IsMatch(item))
                 {
-                    name = regexField.Matches(item)[0].Groups[1].Value.Trim();
-                    fields[name] = desc;
+                    foreach (Group a in regexField.Matches(item)[0].Groups)
+                    {
+                        Console.WriteLine(a.Value);
+                    }
+                    string type = regexField.Matches(item)[0].Groups[1].Value.Trim();
+                    name = regexField.Matches(item)[0].Groups[2].Value.Trim();
+                    fields[name+"__TYPE:"+type] = desc;
                 }
             }
-            string allFields = string.Join(",", fields.Select(it=>$"\"{it.Key}\""));
-            string json = string.Join(",\r\n", fields.Select(it => $"\t{it.Key.ToFirstLetterLower()}:\"{it.Value}\""));
-            string jsTable = string.Join("\r\n", fields.Select(it => $"<el-table-column prop=\"{it.Key.ToFirstLetterLower()}\" label=\"{it.Value}\" align=\"center\"></el-table-column>"));
+            return fields;
+        }
+        private Dictionary<string, string> Line(string[] allLines)
+        {
+            var temp = new List<string>();
+            foreach (var item in allLines)
+            {
+                if (string.IsNullOrEmpty(item)) continue;
+                var line = item.Trim();
+                if (!new Regex("^[^/]+//[^/]+$", RegexOptions.IgnoreCase).IsMatch(line))
+                {
+                    continue;
+                }
+                temp.Add(line);
+            }
+            Dictionary<string, string> fields = new Dictionary<string, string>();
+            string desc = null;
+            string name;
+            foreach (var item in temp)
+            {
+                Regex regexDesc = new Regex("//([\\s\\S]+)", RegexOptions.IgnoreCase);
+                Regex regexField = new Regex("[\\s\\S]+\\.([^,]+)[,]{0,1}[\\s\\S]*//[\\s\\S]+", RegexOptions.IgnoreCase);
+                desc = regexDesc.Matches(item)[0].Groups[1].Value.Trim();
+                var a = regexField.Matches(item)[0];
+                name = regexField.Matches(item)[0].Groups[1].Value.Trim();
+                fields[name] = desc;
+            }
+            return fields;
+        }
+        public void GetResult(Dictionary<string, string> fields)
+        {
+            string allFields = string.Join(",", fields.Select(it => $"\"{GetFieldName(it.Key,out string _)}\""));
+            string json = string.Join("\r\n", fields.Select(it => $"\t{GetFieldName(it.Key, out string type).ToFirstLetterLower()}:\"{it.Value}\",{(string.IsNullOrEmpty(type)?"":"//"+type)}"));
+            string jsTable = string.Join("\r\n", fields.Select(it => $"<el-table-column prop=\"{GetFieldName(it.Key, out string _).ToFirstLetterLower()}\" label=\"{it.Value}\" align=\"center\"></el-table-column>"));
 
-            label1.Text = "{\r\n"+json + "\r\n}\r\n\r\n" + jsTable;
+            label1.Text = "{\r\n" + json + "\r\n}\r\n\r\n" + jsTable;
             textBox2.Text = allFields;
+            label2.Text = string.Join("\r\n", fields.Select(it => $@"   
+        /// <summary>
+        /// {it.Value}
+        /// </summary>
+        public string {GetFieldName(it.Key,out string type)} {{ get; set; }}"));
+        }
+        private string GetFieldName(string str, out string type)
+        {
+            string typeSplit = "__TYPE:";
+            var index = str.IndexOf(typeSplit);
+            if (index==-1)
+            {
+                type = null;
+                return str;
+            }
+            string fieldName = str.Substring(0, index);
+            type = str.Substring(index+typeSplit.Length);
+            return fieldName;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -162,6 +229,16 @@ namespace TD.Concrete.Model.Group.Models.OutModel.Report
         {
 
             Clipboard.SetDataObject(textBox2.Text);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1_TextChanged(null, null);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetDataObject(label2.Text);
         }
     }
 }
